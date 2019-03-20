43790eea91e4eeb490afa9f320ec9d37f2307740
Colin But <colinbut@users.noreply.github.com>
2019-03-20 22:16:34 +0000

    making the test pass with minimal code possible

--- java/src/main/java/partial/word/count/App.java
+++ java/src/main/java/partial/word/count/App.java
@@ -1,7 +1,39 @@
 package partial.word.count;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+
 public class App {
     public String countWords(String file) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(file)))) {
+
+            List<String> words = new ArrayList<>();
+
+            Map<String, Integer> wordCount = new LinkedHashMap<>();
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                words.addAll(Arrays.asList(line.split(" ")));
+
+            }
+
+            words.forEach(word -> wordCount.merge(word, 1, (a, b) -> a + b));
+
+            StringJoiner stringJoiner = new StringJoiner("\n", "", "");
+            wordCount.forEach((word, count) -> stringJoiner.add(word + ": " + count));
+
+            return stringJoiner.toString();
+        } catch (IOException ex) {
+            System.err.print(ex.getLocalizedMessage());
+        }
         return file;
     }
 
