779ecf246bf1ab5e2da41c08ff7e58c1d726ae33
Colin But <colinbut@users.noreply.github.com>
2019-05-11 23:31:57 +0100

    refactor put into source class

---
+++ kotlin/src/main/kotlin/partial/word/count/PartialWordCountK.kt
@@ -0,0 +1,46 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2019 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package partial.word.count
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.lang.StringBuilder
+import java.util.HashMap
+
+class PartialWordCountK {
+
+    fun partialWordCount(inputFile: String): String {
+        val lines = BufferedReader(FileReader(File(inputFile))).lines()
+        val words = lines.findFirst().get().split(" ") as MutableList<String>
+
+        val partialMatches : MutableList<String> = ArrayList()
+        for (word in words) {
+            if (words.stream().filter { w -> w != word }.anyMatch { w -> w.contains(word)}) {
+                partialMatches.add(word)
+            }
+        }
+
+        words.removeAll(partialMatches)
+
+        val wordCount : MutableMap<String, Int> = HashMap()
+
+        words.forEach {
+            if (wordCount[it] != null) {
+                wordCount[it] = wordCount[it]!!.plus(1)
+            } else {
+                wordCount[it] = 1
+            }
+        }
+
+        val resultString = StringBuilder()
+        wordCount.entries.forEach {
+            resultString.append("${it.key}: ${it.value}\n")
+        }
+
+        return resultString.toString()
+    }
+}
--- kotlin/src/test/kotlin/partial/word/count/PartialWordCountTestK.kt
+++ kotlin/src/test/kotlin/partial/word/count/PartialWordCountTestK.kt
@@ -5,58 +5,23 @@
  */
 package partial.word.count
 
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
-import java.lang.StringBuilder
-import java.util.*
-import kotlin.collections.ArrayList
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class PartialWordCountTestK {
 
+    private val partialWordCount : PartialWordCountK = PartialWordCountK()
+
     @Test
     fun `test read input when words are count then output word with their count` () {
         val inputFile = "src/test/resources/input.txt"
 
-        val result = partialWordCount(inputFile)
+        val result = partialWordCount.partialWordCount(inputFile)
 
         assertEquals("A: 1\n" +
                 "material: 1\n" +
                 "maybe: 2\n" +
                 "right: 1\n", result)
-    }
-
-    private fun partialWordCount(inputFile: String): String {
-        val lines = BufferedReader(FileReader(File(inputFile))).lines()
-        val words = lines.findFirst().get().split(" ") as MutableList<String>
-
-        val partialMatches : MutableList<String> = ArrayList()
-        for (word in words) {
-            if (words.stream().filter { w -> w != word }.anyMatch { w -> w.contains(word)}) {
-                partialMatches.add(word)
-            }
-        }
-
-        words.removeAll(partialMatches)
-
-        val wordCount : MutableMap<String, Int> = HashMap()
-
-        words.forEach {
-            if (wordCount[it] != null) {
-                wordCount[it] = wordCount[it]!!.plus(1)
-            } else {
-                wordCount[it] = 1
-            }
-        }
-
-        val resultString = StringBuilder()
-        wordCount.entries.forEach {
-            resultString.append("${it.key}: ${it.value}\n")
-        }
-
-        return resultString.toString()
     }
 
 }
